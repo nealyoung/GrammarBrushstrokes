d066c29bf1871ad3b532d38651b7176c859f24aa
bianxie <bancie@gmail.com>
2014-01-30 21:54:16 -0800

    Added missing definitions to controller.

--- app/controllers/grammar_pages_controller.rb
+++ app/controllers/grammar_pages_controller.rb
@@ -1,4 +1,20 @@
 class GrammarPagesController < ApplicationController
-  def home
-  end
+    def home
+    end
+    
+    def active
+    end
+    
+    def participles
+    end
+    
+    def absolutes
+    end
+    
+    def appositives
+    end
+    
+    def adjectives
+    end
+    
 end
