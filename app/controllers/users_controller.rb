e9e88f8836ed85a44b420568b0c317e5c78054d2
Derek Liang <frostyrusty@gmail.com>
2014-02-05 16:13:19 -0800

    forgot to track some new files (users controller/model and some database files)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "You signed up successfully"
+        flash[:color] = "valid"
+      else
+        flash[:notice] = "Form is invalid"
+        flash[:color] = "invalid"
+      end
+    render "new"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
