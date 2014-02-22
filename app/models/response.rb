84749e8c385074aa077e4d45c1136d448e46690f
Nealon Young <hi@nealyoung.me>
2014-02-21 16:17:25 -0800

    Various changes and improvements

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,8 @@
 class Response < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+  
+  def self.incomplete_responses_for_user(user)
+    user.responses.where(sentence1: nil, sentence2: nil, sentence3: nil)
+  end
 end
