430b8ff3a3123fc4e9ddb8e445f9d22482fb9048
Nealon Young <hi@nealyoung.me>
2014-03-06 11:31:30 -0800

    Removed error causing migrations

--- db/migrate/20140219223438_add_example1_and_example2_to_questions.rb
+++
@@ -1,6 +0,0 @@
-class AddExample1AndExample2ToQuestions < ActiveRecord::Migration
-  def change
-    add_column :questions, :example1, :string
-    add_column :questions, :example2, :string
-  end
-end
--- db/migrate/20140219223829_remove_example1_and_example2_from_questions.rb
+++
@@ -1,6 +0,0 @@
-class RemoveExample1AndExample2FromQuestions < ActiveRecord::Migration
-  def change
-    remove_column :questions, :example1
-    remove_column :questions, :example1
-  end
-end
