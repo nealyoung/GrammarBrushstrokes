561346ac3301c189ac697e402ef15ee7c2a9f0d7
Nealon Young <hi@nealyoung.me>
2014-03-06 17:57:21 -0800

    Removed join table migration

--- db/migrate/20140226065647_create_courses_users_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateCoursesUsersJoinTable < ActiveRecord::Migration
-  def change
-    create_table :courses_users, :id => false do |t|
-      t.integer :course_id
-      t.integer :user_id
-    end
-  end
-end
